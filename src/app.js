126158472c6c6c116b6a9bfcf326628e6721251d
Matias1012 <mng3232@gmail.com>
2025-01-31 22:34:17 -0300

    actualizacion

--- src/app.js
+++ src/app.js
@@ -12,29 +12,8 @@ app.get('/saludo', (req,res)=>{
     res.send("Hola a todos, pero ahora desde express!")
 })
 
-app.post('/api/product', (req,res)=>{
-    const dataRequest =  req.body;
 
-    const listadoTotal =  carrito.items // inicialmente empieza en 0
-
-    const cantidadDeProductos = listadoTotal.length
-
-    const product = {
-        id: cantidadDeProductos + 1,
-        title: dataRequest.title,
-        description: dataRequest.description,
-        code: dataRequest.code,
-        price: dataRequest.price ,
-        status: dataRequest.status ?? true,
-        stock: dataRequest.stock,
-        category: dataRequest.category,
-    }
-
-    listadoTotal.push(product)
-
-    res.send(listadoTotal)
-})
-
+// actualziar producto mediante el id
 app.put ('/api/product/:id', (req,res)=>{
     const productId =  req.params.id;
     const nuevoTitulo = req.body.title;
@@ -47,6 +26,7 @@ app.put ('/api/product/:id', (req,res)=>{
     res.send(products.items)
 })
 
+//borra el producto mediante el id
 app.delete ('/api/product/:id', (req,res)=>{
     const productId =  req.params.id;
 
@@ -55,6 +35,125 @@ app.delete ('/api/product/:id', (req,res)=>{
     })
 
     res.send(productosFiltrados)
+})
+
+
+// crear nuevo producto en producto.json
+app.post('/api/product', (req,res)=>{
+    const nuevoProducto =  req.body;
+
+    const listadoDeProductos =  products.listadoProductos // inicialmente empieza en 0
+
+    const cantidadDeProductos = listadoDeProductos.length
+
+    const product = {
+        id: cantidadDeProductos,
+        title: nuevoProducto.title,
+        description: nuevoProducto.description,
+        code: nuevoProducto.code,
+        price: nuevoProducto.price ,
+        status: nuevoProducto.status ?? true,
+        stock: nuevoProducto.stock,
+        category: nuevoProducto.category,
+    }
+
+    listadoDeProductos.push(product)
+
+    res.send(listadoDeProductos)
+})
+
+
+///  CARRITO ///
+
+// cargar productos en el array del carrito
+app.post('/api/carts', (req,res)=>{
+    const productosRecibidos =  req.body;
+    
+    const carritoCompleto = carrito.listadoDeCarritos[0]
+
+    const listadoProductosEnCarrito =  carritoCompleto.listadoProductos
+
+
+    for (let index = 0; index < productosRecibidos.length; index++) {
+        
+        let producto = {
+            id: index,
+            title: productosRecibidos[index].title,
+            description: productosRecibidos[index].description,
+            code: productosRecibidos[index].code,
+            price: productosRecibidos[index].price ,
+            status: productosRecibidos[index].status ?? true,
+            stock: productosRecibidos[index].stock,
+            category: productosRecibidos[index].category,
+            quantity: productosRecibidos[index].quantity,
+        }
+        listadoProductosEnCarrito.push(producto)
+    }
+
+    res.send(carritoCompleto)
+})
+
+//borra el producto mediante el id del carrito
+app.delete ('/api/carts/:id', (req,res)=>{
+    const productId =  req.params.id;
+
+    const productosFiltrados = products.items.filter((product) => {
+        return product.id !== Number(productId)
+    })
+
+    res.send(productosFiltrados)
+})
+
+// devuelve carrito por el id que le pasamos
+app.get('/api/carts/:cid', (req,res)=>{
+    const carritoId =  req.params.cid;
+
+    const carritoFiltrado = carrito.listadoDeCarritos.filter((carrito) => {
+        return carrito.idCarrito === Number(carritoId)
+    })
+
+    res.send(carritoFiltrado)
+})
+
+// se agrega el id del producto al listado de productos en el carrito que le envio el idCarrito como parametro
+
+app.post('/api/carts/:cid/product/:pid', (req, res) => {
+    const productId = Number(req.params.pid); // pid id producto
+    const carritoId = Number(req.params.cid); // cid id carrito
+
+    const listadoDeProductos =  products.listadoProductos;
+    const listadoDeCarritos = carrito.listadoDeCarritos;
+
+    const carritoSeleccionado = listadoDeCarritos.find(item => item.idCarrito === carritoId)
+
+    const productoSeleccionad = listadoDeProductos.find(item => item.id === productId)
+
+
+    let productRepetido = false;
+
+    if (carritoSeleccionado.listadoProductos.length > 0) {
+
+
+        for (let index = 0; index < carritoSeleccionado.listadoProductos.length; index++) {
+
+            let productoSeleccionado = carritoSeleccionado.listadoProductos[index]
+            if (productoSeleccionado.product === productId) {
+                productRepetido = true;
+                productoSeleccionado.quantity = productoSeleccionado.quantity + 1
+            }
+            
+        }
+    }
+
+    if (productRepetido === false) {
+        carritoSeleccionado.listadoProductos.push({ 
+            product: productoSeleccionad.id, 
+            quantity: productoSeleccionad.quantity 
+        })
+    }
+    
+    res.send(carritoSeleccionado)
+
 })
 
 
